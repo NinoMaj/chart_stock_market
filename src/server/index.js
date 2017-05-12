f1b037cba91b8a37c7710cda5497a7bf2ecf0559
NinoMaj <nino.majder@gmail.com>
2017-05-12 07:32:13 +0200

    Before deploy to Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ import routing from './routing'
 import { WEB_PORT, STATIC_PATH } from '../shared/config'
 import { isProd } from '../shared/util'
 
-require('./models').connect('mongodb://localhost/stocks')
+// require('./models').connect('mongodb://localhost/stocks')
 require('./models').connect(process.env.MONGO_URI)
 
 const app = express()
